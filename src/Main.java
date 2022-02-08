38cfe46ba06280243bc53483cd139b347468fad1
BT_1E10_13 <sample@bths.edu>
2022-02-08 12:23:13 -0500

    update

--- src/Main.java
+++ src/Main.java
@@ -11,20 +11,35 @@ public class Main {
 
         System.out.println(message1.getMachineId());
         System.out.println(message1.getDescription());
+        //true
         System.out.println(message1.containsWord("alert"));
+        //false
         System.out.println(message2.containsWord("disk"));
         System.out.println(message3.containsWord("disk"));
         System.out.println(message4.containsWord("disk"));
+        //true
+        System.out.println(message5.containsWord("disk"));
+        System.out.println(message6.containsWord("disk"));
+        System.out.println(message7.containsWord("disk"));
+        System.out.println(message8.containsWord("disk"));
+
         SystemLog log = new SystemLog();
         log.addLogMessage(message1);
         log.addLogMessage(message2);
         log.addLogMessage(message3);
         log.addLogMessage(message4);
+        //nothing
         System.out.println(log.removeMessages("disk"));
         log.addLogMessage(message5);
         log.addLogMessage(message6);
         log.addLogMessage(message7);
         log.addLogMessage(message8);
+        //messages 5 - 8
         System.out.println(log.removeMessages("disk"));
+        //memory locations of messages 5 - 8
+        System.out.println(message5);
+        System.out.println(message6);
+        System.out.println(message7);
+        System.out.println(message8);
     }
 }
